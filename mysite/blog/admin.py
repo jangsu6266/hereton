075daf7ef3df286979a9cd227827691269a29d0e
Se jeong Kim <somsombenji@naver.com>
2020-08-02 01:11:30 +0900

    해시태그

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Blog, Comment
+from .models import Subject, Comment, Hashtag
 
-admin.site.register(Blog)
+admin.site.register(Subject)
 admin.site.register(Comment)
+admin.site.register(Hashtag)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,38 +1,46 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from django.contrib.auth.decorators import login_required ##
-from .models import Blog,Comment
+from .models import Subject,Comment, Hashtag
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from .form import CommentForm
 
 # Create your views here.
 def subject(request):
-    blogs=Blog.objects
+    blogs=Subject.objects
     return render(request,'subject.html',{'blogs':blogs})
 
 def detail(request,blog_id) :
-    blog_object = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'detail.html', {'blog' : blog_object})
+    blog_object = get_object_or_404(Subject, pk=blog_id)
+    hashtags = blog_object.hashtag.all()
+    return render(request, 'detail.html', {'blog' : blog_object, 'hashtags':hashtags})
 
 def create(request) :
     return render(request, 'create.html')
 
 def new(request) :
-    blog = Blog()
-    blog.title = request.POST['title']
-    blog.body = request.POST['body']
-    blog.pub_date = timezone.datetime.ow()
+    blog = Subject()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.professor = request.GET['professor']
     blog.save()
+
+    hashtags=request.GET['hashtags']
+    hashtag=hashtags.split(",")
+    for tag in hashtag:
+        ht = Hashtag()
+        ht.name = tag
+        ht.save()
+        blog.hashtag.add(ht)
 
     return redirect('/blog/' +str(blog.id))
 
 def delete(request, blog_id):
-    blogs = Blog.objects.get(pk= blog_id)
+    blogs = Subject.objects.get(pk= blog_id)
     blogs.delete()
     return redirect('subject')
 
 def edit(request, blog_id):
-    blog_edit = Blog.objects.get(pk=blog_id)
+    blog_edit = Subject.objects.get(pk=blog_id)
     return render(request, 'edit.html',{'blog':blog_edit})
 
 def home(request):
@@ -42,18 +50,17 @@ def home(request):
 
 @csrf_exempt
 def update(request, blog_id) :
-    blog = Blog.objects.get(pk-blog_id)
+    blog = Subject.objects.get(pk=blog_id)
     blog.title = request.POST['title']
     blog.body = request.POST['body']
     blog.pub_date = timezone.datetime.now()
     blog.save()
-    return redirect('home')
+    return redirect('subject')
 
 
 #댓글
-@login_required
 def add_comment_to_post(request, blog_id):
-    blog = get_object_or_404(Blog, pk=blog_id)
+    blog = get_object_or_404(Subject, pk=blog_id)
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
